f3dfeb5c91b2e7fbdcbdf90f989aeffe5cd8952b
badrikh <s1232166@stu.najah.edu>
2025-01-01 18:11:13 +0200

    navbar aboutus

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Homepage from "./components/Homepage/Homepage";
+import "./App.css";
+
+
+function App() {
+  return (
+    
+    <div className="App-container">
+      
+        <div className="Homepage">
+          <Homepage />
+         
+        </div>
+
+        </div>
+  );
+}
+
+export default App;
