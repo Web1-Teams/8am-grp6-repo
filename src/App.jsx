7768e368ff98eb68a030b613941a5adc977cd6fb
tamara <tamarahisham994@gmail.com>
2025-01-01 21:54:16 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import './App.css';
+import PageContent from './components/infection/PageContent.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/infection/DoctorSection.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./DoctorSection.css";
+
+const DoctorsList = ({ doctors }) => {
+  return (
+    <div className="doctors-container">
+      {doctors.map((doctor, index) => (
+        <div className="doctor-card" key={index}>
+          <img src={doctor.image} alt={doctor.name} className="doctor-image" />
+          <h3 className="doctor-name">{doctor.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const DoctorsSection = () => {
+  const [doctors, setDoctors] = useState([
+    { name: "Dr. Jehad ismail", image: "src/assets/جهاد اسماعيل.png" },
+    { name: "Dr. Jawad zeyada", image: "src/assets/جواد زيادة.png" },
+  ]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return <div>Loading doctors...</div>;
+  }
+
+  return <DoctorsList doctors={doctors} />;
+};
+
+export default DoctorsSection;
