c65d01e0b6ce6c7985d50ffbd3e66d0e488a2f0a
nabeelayyash <s12326516@stu.najah.edu>
2025-01-01 18:10:17 +0200

    First Step

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CardiologySection from './components/CardiologySection';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CardiologySection />
+    </div>
+  );
+}
+export default App; 
