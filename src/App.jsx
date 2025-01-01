7c603d0a88949276db19b13596466cfe31c9a6f1
nooryaseen428 <s12323629@stu.najah.edu>
2025-01-01 18:35:28 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppointmentForm from './components/AppointmentForm';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <AppointmentForm />
+    </div>
+  );
+};
+
+export default App;
