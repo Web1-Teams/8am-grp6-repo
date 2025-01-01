6851d05d49082a1cfb578541fd68394ab2c8599d
jamal5274 <jamal.ilaiwi@gmail.com>
2025-01-01 17:29:10 +0200

    pharmcy

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PageContent from './components/pharmcyCOM/PageContent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
