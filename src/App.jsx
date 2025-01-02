9cf6a86d10b3c994ee12d6c2a3e1b3f4d6cfbc1c
rehamedily <s12327091@stu@najah.edu>
2025-01-02 14:57:35 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import './App.css';
+import PageContent from './components/physical/PageContent.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
