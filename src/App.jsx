d225cb2d57693221a9388168e385b8c39fce2b43
laianmhmoud11 <s12323942@stu.najah.edu>
2025-01-04 00:19:56 +0200

    created homepage, footer, feedback

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Homepage from "C:\\Users\\إلكتروني\\OneDrive\\سطح المكتب\\8am-grp6-repo\\src\\components\\Homepage\\Homepage.jsx";
+import Feedback from "C:\\Users\\إلكتروني\\OneDrive\\سطح المكتب\\8am-grp6-repo\\src\\components\\Footer\\Footer.jsx";
+import Footer from "C:\\Users\\إلكتروني\\OneDrive\\سطح المكتب\\8am-grp6-repo\\src\\components\\Footer\\Footer.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Homepage">
+        <Homepage />
+      </div>
+      <div className="Feedback">
+        <Feedback />
+      </div>
+      <div className="Footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
