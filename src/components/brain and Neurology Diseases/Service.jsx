1e48feb75d4ff08180faf9055f0fddf5c6cd6b37
NaeemSaadeh <s12323447@stu.najah.edu>
2025-01-03 21:36:45 +0200

    EDIT

---
+++ src/components/brain and Neurology Diseases/Service.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import "./Service.css";
+
+const ServicesList = ({services}) => {
+  const [displayedServices, setDisplayedServices] = useState(services);
+
+  useEffect(() => {
+    setDisplayedServices(services);
+  }, [services]);
+
+  return (
+    <ul className="services-list">
+      {displayedServices.map((service, index) => (
+        <li key={index}>{service}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ServicesList;
