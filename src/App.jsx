ffe30aef7ae728e148e71f0473bd75f175aa5c33
nabeelayyash <s12326516@stu.najah.edu>
2025-01-04 12:28:07 +0200

    Edit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import PageContent from './components/gastrointesti/PageContent';
+import Homepage from './components/gastrointesti/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
