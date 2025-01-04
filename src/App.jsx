5b8b5f018c1facf72ff00164068a86668d5804b1
AmeedToqan <s12324830@stu.najah.edu>
2025-01-04 13:20:23 +0200

    Final

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Homepage from './components/brain and Neurology Diseases/Homepage';
+import PageContent from './components/brain and Neurology Diseases/PageContent';
+function App() {  
+  return (
+    <div className="App">
+       <Homepage /> 
+       <PageContent />
+    </div>
+  );
+}
+export default App
