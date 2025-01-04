61570170b28139b8ab48a74ba4796d240adf3d4b
nabeelayyash <s12326516@stu.najah.edu>
2025-01-04 12:22:28 +0200

    Edit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import PageContent from './components/internal-Medicine/Internal-Medicine/PageContent';
+import Homepage from './components/internal-Medicine/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      < Homepage/>
+      <PageContent />
+    </div>
+  );
+}
+
+export default App;
