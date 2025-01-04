60424092ff4e8b93cff92fb36906c895c4688522
badrikh <s1232166@stu.najah.edu>
2025-01-04 11:27:34 +0200

    about us style

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -77,14 +77,29 @@ type="video/mp4" />
       </section>
       <br />
       <div className="content-sub"><h2>About An-Najah Hospital</h2>
-        <p>
-          Our website is an innovative platform designed to enhance the healthcare experience by providing
-          comprehensive services that make accessing medical information simple and efficient. Through the
-          website, you can explore a detailed list of available doctors, review their specialties and medical
-          expertise, and select the doctor best suited to your health needs. The platform also offers accurate
-          information about doctors' schedules and hospital services, with the convenience of booking
-          appointments online.
-        </p><br />
+         <p>
+         Our website is an innovative platform designed to enhance the
+                    healthcare experience by providing comprehensive services that
+                    make accessing medical information simple and efficient.
+                     Through the website, you can explore a detailed list of
+         available doctors, review their specialties and medical
+        expertise, and select the doctor best suited to your health
+         needs. The platform also offers accurate
+        information about doctors schedules and hospital services,
+         with the convenience of booking
+        appointments online.  
+                    In addition, users can access helpful health tips and educational  
+        resources to support their wellness journey. The website also provides  
+        timely updates on hospital news, events, and community programs.  
+        For emergencies, the platform ensures you can quickly find critical contact  
+        details and essential guidance.
+                    
+
+         </p>
+        
+        
+          
+        <br />
         
         <div className="addition" ><h4>Insurance Company</h4><br />
         <img
